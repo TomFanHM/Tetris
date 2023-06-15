a4c038efe1e0003552d7768ccfb674b3d9fe18fb
Tom Fan <119341778+TomFanHM@users.noreply.github.com>
2023-06-15 05:58:27 +0000

    update

--- my-app/src/canvas/Interface.tsx
+++ my-app/src/canvas/Interface.tsx
@@ -9,15 +9,62 @@ export const Interface = () => {
     const [gameOver, setGameOver] = useState<boolean>(false);
     const [gameReset, setGameReset] = useState<boolean>(false);
     const [gameMute, setGameMute] = useState<boolean>(false);
-
+  
+    const handleOnClickStart = () => {
+      setGameStart(true);
+    };
+  
+    const handleOnClickNewGame = () => {
+      setGameStart(true);
+      setGameReset((prev) => !prev);
+      setGameOver(false);
+    };
+  
+    const handleOnClickMute = () => {
+      setGameMute((prev) => !prev);
+    };
+  
+    const handleOnClickPause = () => {
+      setGameStart(false);
+    };
+  
+    const handleGameOver = () => {
+      setGameStart(false);
+      setGameOver(true);
+    };
+  
     return (
-        <>
-            <Html fullscreen>
-                <button className={buttonStyle} onClick={() => setGameStart(true)}>Start</button>
-                <button className={buttonStyle} onClick={() => setGameReset((prev) => !prev)}>New Game</button>
-            </Html>
-            <Playfield gameStart={gameStart} gameReset={gameReset} />
-        </>
-
-    )
-}
+      <>
+        <Html fullscreen>
+          {!gameStart && (
+            <>
+              <button
+                className={buttonStyle}
+                onClick={handleOnClickStart}
+                disabled={gameOver}
+              >
+                Start
+              </button>
+              <button className={buttonStyle} onClick={handleOnClickNewGame}>
+                New Game
+              </button>
+              <button className={buttonStyle} onClick={handleOnClickMute}>
+                {gameMute ? "Unmute" : "Mute"}
+              </button>
+            </>
+          )}
+          {gameStart && (
+            <button className={buttonStyle} onClick={handleOnClickPause}>
+              Pause
+            </button>
+          )}
+        </Html>
+        <Playfield
+          gameStart={gameStart}
+          gameReset={gameReset}
+          handleGameOver={handleGameOver}
+        />
+      </>
+    );
+  };
+  
--- my-app/src/canvas/useKeyboardControls.ts
+++ my-app/src/canvas/useKeyboardControls.ts
@@ -1,42 +1,42 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const useKeyboardControls = () => {
-    const keys = {
-        ArrowUp: 'forward',
-        ArrowDown: 'backward',
-        ArrowLeft: 'left',
-        ArrowRight: 'right',
-        Space: 'space',
-        ShiftLeft: "shift"
-    }
+  const keys = {
+    ArrowUp: "forward",
+    ArrowDown: "backward",
+    ArrowLeft: "left",
+    ArrowRight: "right",
+    Space: "space",
+    ShiftLeft: "shift",
+  };
 
-    const moveFieldByKey = (key: string) => keys[key as keyof typeof keys]
+  const moveFieldByKey = (key: string) => keys[key as keyof typeof keys];
 
-    const [movement, setMovement] = useState({
-        forward: false,
-        backward: false,
-        left: false,
-        right: false,
-        space: false,
-        shift: false
-    })
+  const [movement, setMovement] = useState({
+    forward: false,
+    backward: false,
+    left: false,
+    right: false,
+    space: false,
+    shift: false,
+  });
 
-    useEffect(() => {
-        const handleKeyDown = (e: KeyboardEvent) => {
-            setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }))
-        }
-        const handleKeyUp = (e: KeyboardEvent) => {
-            setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }))
-        }
-        document.addEventListener('keydown', handleKeyDown)
-        document.addEventListener('keyup', handleKeyUp)
-        return () => {
-            document.removeEventListener('keydown', handleKeyDown)
-            document.removeEventListener('keyup', handleKeyUp)
-        }
-    }, [])
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: true }));
+    };
+    const handleKeyUp = (e: KeyboardEvent) => {
+      setMovement((m) => ({ ...m, [moveFieldByKey(e.code)]: false }));
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
-    return movement
-}
+  return movement;
+};
 
-export default useKeyboardControls
+export default useKeyboardControls;
